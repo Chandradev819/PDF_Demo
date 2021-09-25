a08733c11d1907ea8ddf43ddba51c3282f086f66
Chandradev <chandradev_ps@dellteam.com>
2021-09-25 23:29:59 +0530

    Add project files.

---
+++ View/Home.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+
+
+namespace PDF_Demo.View
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            string filepath = @"C:\Users\chandradev_ps\Desktop\Chandradev\Demo.pdf";
+            string text = ReadFile(filepath);
+            Response.Write(text);
+        }
+
+        public string ReadFile(string pdfPath)
+        {
+            var pageText = new StringBuilder();
+            using (PdfDocument pdfDocument = new PdfDocument(new PdfReader(pdfPath)))
+            {
+                var pageNumbers = pdfDocument.GetNumberOfPages();
+                for (int i = 1; i <= pageNumbers; i++)
+                {
+                    LocationTextExtractionStrategy strategy = new LocationTextExtractionStrategy();
+                    PdfCanvasProcessor parser = new PdfCanvasProcessor(strategy);
+                    parser.ProcessPageContent(pdfDocument.GetFirstPage());
+                    pageText.Append(strategy.GetResultantText());
+                }
+            }
+            return pageText.ToString();
+        }
+    }
+}
---
+++ View/WordReader.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+namespace PDF_Demo.View
+{
+    public partial class WordReader : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (PdfDocument document = PdfDocument.Open(@"C:\Users\chandradev_ps\Desktop\Chandradev\Demo.pdf"))
+            {
+                foreach (Page page in document.GetPages())
+                {
+                    string pageText = page.Text;
+
+                    foreach (Word word in page.GetWords())
+                    {
+                        if (word.Text== "State")
+                        {
+                            Response.Write(word.Text);
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
